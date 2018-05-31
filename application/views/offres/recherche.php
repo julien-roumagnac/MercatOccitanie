92fbc94c9f1146eed278a5104cb1fa9dc04e232d
julien <julienroumagnac1@gmail.com>
2018-05-31 18:22:00 +0200

    r

--- application/views/offres/recherche.php
+++ application/views/offres/recherche.php
@@ -34,7 +34,7 @@
             </div>
         </div>
 
-        <button type="submit" class="btn btn-light"><img width="20%" src="<?php echo base_url('assets/icons/loupe.svg')?>"></button>
+        <button type="submit" class="btn btn-light"><img width="20%" src="<?php echo base_url('assets/icons/white-loupe.svg')?>"></button>
 
 
     </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>MercatOccitanie</title>
+         <link rel="icon" href="<?php echo base_url('assets/icons/soccer.png');?>">
     </head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="stylesheet"  href="<?php echo site_url().'assets/css/style.css';?>">
