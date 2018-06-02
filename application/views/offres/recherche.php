411972686251db861cfc2dcdfff6a5598b291c8c
julien <julienroumagnac1@gmail.com>
2018-06-02 09:42:42 +0200

    rech

--- application/views/offres/recherche.php
+++ application/views/offres/recherche.php
@@ -40,37 +40,46 @@
     </div>
     </form>
 </div>
-<div class="container">
+<div class="container text-center" >
+            <h2>Resultats </h2>
+    <div class="container card mt-3 " >
 
-    <ul class="list-group list-group-flush">
+
+        <ul class="list-group list-group-flush py-3">
 
 
-        <?php foreach($offres as $offre) : ?>
-            <li class="list-group-item">
-                <div class="card mb-3">
-                    <div class="card-header cardtitle row text-center">
-                        <div class="col-md-2 col-sm-4">
-                            <?php if($offre['id_user']===$user):?>
+            <?php foreach($offres as $offre) : ?>
+                <li class="list-group-item">
+                    <div class="card mb-3">
+                        <div class="card-header cardtitle row text-center" style="margin-left: 0px ; margin-right: 0px">
 
-                                 <a class="float-left" href=" <?php echo base_url('/offres/delete/'.$offre['offres_id']) ;?>" >
-                                     <img class="float-left" width="20%" src="<?php echo site_url('assets/icons/cancel2.svg');?>">
-                                </a>
-                            <?php endif?>
+                            <div class="col-md-4 col-sm-6">Poste : <h6><?php echo $offre['poste']; ?></h6></div>
+                            <div class="col-md-4 col-sm-6">niveau : <h6><?php echo $offre['division'];  ?></h6></div>
+                            <div class="col-md-4 col-sm-10">
+                                <?php
+                                if ($offre['id_user']===$user) {
+                                    echo  '<a class="" href="'.base_url('/offres/delete/'.$offre['offres_id']).'" >
+                                        <img class="" width="25px" height="25px"  src="'.site_url('assets/icons/cancel2.svg').'"></a>';
+                                } else {
+                                    echo 'En savoir plus sur l\'auteur  <a href="'.site_url().'profil/view/'.$offre['id_user'].'\"  >
+                                    <img  width="25px" height="25px" src="'.site_url('assets/icons/navarrow.svg').'"> </a>';
+
+                                }
+                                ?>
+
+
+                            </div>
+
                         </div>
-                        <div class="col-md-3 col-sm-4">Poste: <?php echo $offre['poste']; ?></div>
-                        <div class="col-md-3 col-sm-4">niveau : <?php echo $offre['division'];  ?></div>
-                        <div class="col-md-4 col-sm-10">En savoir plus sur l'auteur  <a href="<?php echo site_url().'profil/view/'.$offre['id_user'];  ?>"> <img  width="25px" height="25px" src="<?php echo site_url('assets/icons/navarrow.svg');?>"></a></div>
-
+                        <div class="card-body">
+                            <h5 class="card-title">Description</h5>
+                            <p class="card-text">  <?php echo word_limiter($offre['description'],20); ?> </p>
+                            <a href="<?php echo site_url('/offres/view/'.$offre['offres_id']) ;?>" class="btn">plus</a>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Description</h5>
-                        <p class="card-text">  <?php echo word_limiter($offre['description'],20); ?> </p>
-                        <a href="<?php echo site_url('/offres/view/'.$offre['offres_id']) ;?>" class="btn">plus</a>
-                    </div>
-                </div>
-            </li>
-        <?php endforeach;?>
-    </ul>
+                </li>
+            <?php endforeach;?>
+        </ul>
 
-</div>
+    </div>
 </div>
