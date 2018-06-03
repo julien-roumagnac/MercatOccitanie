eb50d4976dc693cb68a36621fac434d9a3973849
julien <julienroumagnac1@gmail.com>
2018-06-03 19:14:01 +0200

    no

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$route['Profil/view/(:any)']='Profil/$1';
+
 $route['offres/create']='offres/create';
 $route['offres']='offres/index';
 $route['default_controller'] = 'Accueil/connexion';
