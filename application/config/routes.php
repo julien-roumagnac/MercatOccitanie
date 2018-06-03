39d270f8571c675b61c16ff1cfdb3bba57ed9401
julien <julienroumagnac1@gmail.com>
2018-06-03 19:02:20 +0200

    rout2

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$route=['profil/(:any)']='profil/view/$1'
+$route['profil/(:any)']='profil/view/$1'
 $route['offres/create']='offres/create';
 $route['offres']='offres/index';
 $route['default_controller'] = 'Accueil/connexion';
-$route['home']='pages/view';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
